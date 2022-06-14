ac7eee19a35f6915a67ae20513f32934bcc3dc4d
faozybg <segunfaozan@gmail.com>
2022-06-14 04:49:36 -0700

    Simple Movie API, ready to be connected to a frontend application

---
+++ server/models/movie.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : 'This field is required'
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    category : {
+        type : Array,
+        required : true
+    },
+    thumbnail : {
+        type : Array,
+        required : true
+    }
+});
+
+movieSchema.index({"$**" : 'text'});
+
+
+module.exports = mongoose.model('Movie', movieSchema);
